8e071b84da3ebe4e78131c906e286f7bb933d32c
ZoeLi <zoeli@tstartel.com>
2020-05-16 16:31:43 +0800

    Initial Commit

---
+++ diceGame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  diceGame
+//
+//  Created by zoeli on 2020/5/16.
+//  Copyright © 2020 zoeli. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
